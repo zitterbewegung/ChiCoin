14edfb10ca7b63d29ff70f0be0d47f95168b83a1
lepipele <lepipele@users.noreply.github.com>
2018-03-16 12:48:33 -0500

    Invoking smart contract to check if registered

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', function() {
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-      // Use Mist/MetaMask's provider
-      web3js = new Web3(web3.currentProvider);
-    } else {
-      console.log('No web3? You should consider trying MetaMask!')
-      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3js = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
 
-    startApp();
-  })
+  startApp();
+});
 
 function startApp() {
   web3.version.getNetwork((err, netId) => {
@@ -33,5 +33,10 @@ function startApp() {
       default:
         console.log('This is an unknown network.')
     }
-  })
+  });
+
+  var cbpContract = web3.eth.contract(contractAbi).at(contractAddress);
+  cbpContract.isMember(function(err, res) {
+    console.log(res);
+  });
 }
