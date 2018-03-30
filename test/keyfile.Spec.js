6f5fa9a2ec4a3a40a2e45bcfa23515e13b129b5a
Prem Kumar <premkumar2306@gmail.com>
2018-03-29 21:44:07 -0500

    added test case to encrypt and decrypt private key using password - AES256

---
+++ test/keyfile.Spec.js
@@ -0,0 +1,26 @@
+require("babel-polyfill");
+import assert from "assert";
+import config from "../src/lib/config.js";
+import Wallet from "../src/lib/wallet.js";
+import keyfile from "../src/lib/aeskeyfile.js";
+
+let password;
+let mnemonic;
+
+beforeEach(async () => {
+  password = "sincityofmyadventure";
+  mnemonic = "patch mistake raven tonight bottom addict balcony celery merit relax derive soda";
+  
+});
+
+describe("Keyfile", () => {
+  it.only("should be able to encode file and decode", () => {
+    const webWallet  = Wallet.restoreFromMnemonic(mnemonic, password);
+    const privateKey = webWallet.getPrivKey();
+    const encodedData = keyfile.encode(privateKey, password);
+    const decodedData = keyfile.decode(encodedData, password);
+    assert.ok(encodedData);
+    assert.deepStrictEqual(privateKey, decodedData, "should be able to encode and decode");
+  });
+
+});
